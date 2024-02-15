62188c93bfe3eaadb8dc648db813070e175a28a2
Faroeq <alves_1044@hotmail.com>
2024-02-15 19:56:34 +0100

    Add express with Joke endpoint, harcoded prompt

--- server/server.js
+++ server/server.js
@@ -1,11 +1,28 @@
 import { ChatOpenAI } from "@langchain/openai";
+import express from "express";
 
-const model = new ChatOpenAI({
-  azureOpenAIApiKey: process.env.AZURE_OPENAI_API_KEY,
-  azureOpenAIApiVersion: process.env.OPENAI_API_VERSION,
-  azureOpenAIApiInstanceName: process.env.INSTANCE_NAME,
-  azureOpenAIApiDeploymentName: process.env.ENGINE_NAME,
+const app = express();
+const port = process.env.EXPRESS_PORT || 3000;
+
+app.get("/joke", async (req, res) => {
+  const model = new ChatOpenAI({
+    azureOpenAIApiKey: process.env.AZURE_OPENAI_API_KEY,
+    azureOpenAIApiVersion: process.env.OPENAI_API_VERSION,
+    azureOpenAIApiInstanceName: process.env.INSTANCE_NAME,
+    azureOpenAIApiDeploymentName: process.env.ENGINE_NAME,
+  });
+
+  const joke = await model.invoke("How dou center a div");
+
+  res.json({
+    message: joke.content,
+  });
+
+  next();
 });
 
-const joke = await model.invoke("Tell me a Javascript joke!");
-console.log(joke.content);
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
+
+// console.log(joke.content);
