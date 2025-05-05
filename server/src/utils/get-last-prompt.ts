673bc2ef8e4f2f9b60af950a8514bf8817a84a30
Faroeq (laptop) <faroeqalves@gmail.com>
2025-05-05 13:58:50 +0200

    Add comment for prompt utility

--- server/src/utils/get-last-prompt.ts
+++ server/src/utils/get-last-prompt.ts
@@ -1,3 +1,14 @@
+/**
+ * Extracts the last prompt from an array of messages.
+ *
+ * @param messages - An array of message entries
+ * @returns The content of the last prompt as a string
+ * @throws {TypeError} Will throw if messages array is empty
+ *
+ * @example
+ * const messages = [["human", "first"], ["ai", "second"]];
+ * const lastPrompt = getLastPrompt(messages); // returns "second"
+ */
 export const getLastPrompt = (messages: []): string => {
   const lastPrompt = messages[messages.length - 1];
   return lastPrompt[1];
